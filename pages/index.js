d3e823c2b2bf842aae916b074715e10fb78e19ee
YellowFlash2012 <cyclop9@hotmail.com>
2022-09-29 05:56:45 +0100

    created the fake product data, created the productitem and rendered it on the home page, learned how to write tailwind css in the globals.css and have it applied to individual elements in their respective components. I'm kindda starting to like tailwind

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import Layout from "../components/Layout";
+import ProductItem from "../components/ProductItem";
+import { data } from "../utils/data";
 
 
 export default function Home() {
@@ -7,7 +9,11 @@ export default function Home() {
       
 
       <Layout title="Home">
-        Home page
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+          {data.products.map(product => (
+            <ProductItem product={product} key={product.slug} />
+          ))}
+        </div>
       </Layout>
 
       
